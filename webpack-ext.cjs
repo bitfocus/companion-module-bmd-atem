a54690beec64bb5e55a23c4aaf3b53b8845b6a23
Julian Waller <git@julusian.co.uk>
2022-07-04 00:48:31 +0100

    wip

--- webpack-ext.cjs
+++ webpack-ext.cjs
@@ -1,12 +1,13 @@
 module.exports = {
 	entry: {
-		//
-		atemSocketChild: './node_modules/atem-connection/dist/lib/atemSocketChild.js',
+		// atemSocketChild: './node_modules/atem-connection/dist/lib/atemSocketChild.js',
 	},
 	externals: [
 		{
-			'@julusian/freetype2': 'commonjs @julusian/freetype2',
+			// '@julusian/freetype2': 'commonjs @julusian/freetype2',
+			// TODO: This is not good for number of files on disk, but is easy to implement
+			'atem-connection': 'commonjs atem-connection',
 		},
-		/^(atemSocketChild)$/i,
+		// /^(atemSocketChild)$/i,
 	],
 }
