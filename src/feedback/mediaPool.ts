3988e8e4a7519c303e4335473fe8312fe4a1a7f0
Julian Waller <git@julusian.co.uk>
2024-08-15 21:38:40 +0100

    wip

--- src/feedback/mediaPool.ts
+++ src/feedback/mediaPool.ts
@@ -1,14 +1,18 @@
 import { AtemMediaPlayerPicker, AtemMediaPlayerSourcePicker } from '../input.js'
 import type { ModelSpec } from '../models/index.js'
-import type { MyFeedbackDefinitions } from './types.js'
+import type { MyAdvancedFeedbackEvent, MyFeedbackDefinitions } from './types.js'
 import { FeedbackId } from './FeedbackId.js'
-import type { StateWrapper } from '../state.js'
+import type { MediaPoolPreviewOptions, StateWrapper } from '../state.js'
+import type { MyOptionsHelper } from '../common.js'
 
 export interface AtemMediaPoolFeedbacks {
 	[FeedbackId.MediaPoolPreview]: {
 		mediaplayer: number
 		// TODO - clip vs still?
 		source: number
+
+		position: 'top' | 'center' | 'bottom'
+		crop: 'none' | 'left' | 'center' | 'right'
 	}
 }
 
@@ -29,9 +33,43 @@ export function createMediaPoolFeedbacks(
 			options: {
 				mediaplayer: AtemMediaPlayerPicker(model),
 				source: AtemMediaPlayerSourcePicker(model, state.state),
+
+				crop: {
+					id: 'crop',
+					type: 'dropdown',
+					label: 'Crop',
+					default: 'none',
+					choices: [
+						{ id: 'none', label: 'None' },
+						{ id: 'left', label: 'Left' },
+						{ id: 'center', label: 'Center' },
+						{ id: 'right', label: 'Right' },
+					],
+				},
+				position: {
+					id: 'position',
+					type: 'dropdown',
+					label: 'Position',
+					default: 'center',
+					choices: [
+						{ id: 'top', label: 'Top' },
+						{ id: 'center', label: 'Center' },
+						{ id: 'bottom', label: 'Bottom' },
+					],
+					isVisible: (options) => options['crop'] !== 'none',
+				},
 			},
-			callback: ({ id, options }) => {
+			callback: async ({ id, options, image }) => {
 				console.log('check feedback', id, options.getJson())
+
+				const previewOptions = parsePreviewOptions(options, image)
+				if (!previewOptions) return {}
+
+				const imageBuffer = await state.mediaPoolCache.getPreviewImage(options.getPlainNumber('source'), previewOptions)
+				if (imageBuffer) {
+					return imageBuffer
+				}
+
 				// TODO
 				return {
 					text: 'Loading...',
@@ -41,15 +79,29 @@ export function createMediaPoolFeedbacks(
 				}
 			},
 			subscribe: ({ id, options }) => {
-				state.mediaPoolCache.subscribe(options.getPlainNumber('source'), id, {
-					// TODO - options
-					crop: 'none',
-					position: 'center',
-				})
+				// const previewOptions = parsePreviewOptions(options)
+				// if (!previewOptions) return
+
+				state.mediaPoolCache.subscribe(options.getPlainNumber('source'), id)
 			},
 			unsubscribe: ({ id, options }) => {
 				state.mediaPoolCache.unsubscribe(options.getPlainNumber('source'), id)
 			},
 		},
+	}
+}
+
+function parsePreviewOptions(
+	options: MyOptionsHelper<AtemMediaPoolFeedbacks['mediaPoolPreview']>,
+	imageProps: MyAdvancedFeedbackEvent<unknown>['image']
+): MediaPoolPreviewOptions | null {
+	if (!imageProps) return null
+
+	return {
+		crop: options.getPlainString('crop'),
+		position: options.getPlainString('position'),
+
+		buttonHeight: imageProps.height,
+		buttonWidth: imageProps.width,
 	}
 }
