28f92dc83f16b3e602f6fbe1c6a492b9cf0d30d4
Julian Waller <git@julusian.co.uk>
2024-09-10 16:56:26 +0100

    chore: replace ts-node with tsx

--- dump-model.ts
+++ dump-model.ts
@@ -5,8 +5,8 @@ import { GetParsedModelSpec } from './src/models/index.js'
 
 const args = process.argv.slice(2)
 if (args.length < 1) {
-	console.log('Usage: yarn ts-node-esm dump-model.ts <atem-ip>')
-	console.log('eg: yarn ts-node-esm dump-model.ts 10.42.13.99')
+	console.log('Usage: yarn tsx dump-model.ts <atem-ip>')
+	console.log('eg: yarn tsx dump-model.ts 10.42.13.99')
 	process.exit()
 }
 
